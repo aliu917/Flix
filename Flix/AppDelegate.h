6823a0b8d754fc9d51130fcce14aea078395d676
Angela Liu <aliu18@fb.com>
2019-06-26 11:03:11 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by aliu18 on 6/26/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by aliu18 on 6/26/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
