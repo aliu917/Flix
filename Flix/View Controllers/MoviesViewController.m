e30601fc279e78bafa9c303670dea2eb80ea7aad
Angela Liu <aliu18@fb.com>
2019-06-26 14:43:11 -0700

    Completed basic working structure of movie title and synopses but without images yet.

---
+++ Flix/View Controllers/MoviesViewController.m
@@ -0,0 +1,78 @@
+//
+//  MoviesViewController.m
+//  Flix
+//
+//  Created by aliu18 on 6/26/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import "MoviesViewController.h"
+#import "MovieCell.h"
+
+@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *movies;
+
+@end
+
+@implementation MoviesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tableView.dataSource = self;
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            NSLog(@"%@", dataDictionary);
+            // TODO: Get the array of movies
+            NSArray *movies = dataDictionary[@"results"];
+            self.movies = movies;
+            for (NSDictionary *movie in movies) {
+                NSLog(@"%@", movie[@"title"]);
+            }
+            // TODO: Store the movies in a property to use elsewhere
+            // TODO: Reload your table view data
+            
+            [self.tableView reloadData];
+        }
+    }];
+    [task resume];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
+    
+    NSDictionary *movie = self.movies[indexPath.row];
+    cell.titleLabel = movie[@"title"];
+    cell.synopsisLabel.text = movie[@"overview"];
+    //cell.textLabel.text = movie[@"title"];
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
